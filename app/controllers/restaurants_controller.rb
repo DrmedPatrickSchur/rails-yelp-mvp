824ceaebbb5eafaea82c79c7793178d5b6156936
Patrick Schur <patrick.schur@icloud.com>
2023-02-22 17:07:54 +0100

    for now

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,19 +1,19 @@
 class RestaurantsController < ApplicationController
 
-def index
-  @restaurants = Restaurant.all
-end
+  def index
+    @restaurants = Restaurant.all
+  end
 
-def show
-  @restaurant = Restaurant
-end
+  def show
+    @restaurant = Restaurant
+  end
 
-def new
-  @restaurant = Restaurant.new
-  redirect_to new_restaurant_path(@restaurant)
-end
+  def new
+    @restaurant = Restaurant.new
+    redirect_to new_restaurant_path(@restaurant)
+  end
 
-def create
-end
+  def create
+  end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,12 @@
 class ReviewsController < ApplicationController
 
-def new
-  @review = Review.new
-end
+  def new
+    restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
 
-def create
-  @review = Review.new(review_params)
-  redirect_to restaurant_path(@restaurant)
-end
+  def create
+    @review = Review.new(review_params)
+    redirect_to restaurant_path(@restaurant)
+  end
 end
